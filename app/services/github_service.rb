e8f071ecc214796afea40d3fefd7b8857917a849
Katie Keel <akatiekeel@gmail.com>
2017-08-24 14:48:17 -0600

    Probably should test at this point

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -6,10 +6,7 @@ class GithubService
   def initialize(user)
     @user = user
     @conn = Faraday.new(url: "https://api.github.com") do |faraday|
-      faraday.headers["X-API-KEY"] = ENV["GITHUB_CLIENT_ID"]
-      faraday.adapter Faraday.default_adapter
-    end
-    @events_conn = Faraday.new(url: "https://api.github.com") do |faraday|
+      faraday.authorization :Token, User.find_by(screen_name: user).oauth_token
       faraday.headers["X-API-KEY"] = ENV["GITHUB_CLIENT_ID"]
       faraday.headers["Accept"] = "application/vnd.github.cloak-preview"
       faraday.adapter Faraday.default_adapter
@@ -26,7 +23,7 @@ class GithubService
   end
 
   def find_starred_repos
-    get_url("/users/#{user}/starred?client_id=#{ENV["GITHUB_CLIENT_ID"]}&client_secret=#{ENV["GITHUB_CLIENT_SECRET"]}")
+    get_url("/users/#{user}/starred")
   end
 
   def self.find_followers(user)
@@ -34,7 +31,7 @@ class GithubService
   end
 
   def find_followers
-    get_url("/users/#{user}/followers?client_id=#{ENV["GITHUB_CLIENT_ID"]}&client_secret=#{ENV["GITHUB_CLIENT_SECRET"]}")
+    get_url("/users/#{user}/followers")
   end
 
   def self.find_following(user)
@@ -42,7 +39,7 @@ class GithubService
   end
 
   def find_following
-    get_url("/users/#{user}/following?client_id=#{ENV["GITHUB_CLIENT_ID"]}&client_secret=#{ENV["GITHUB_CLIENT_SECRET"]}")
+    get_url("/users/#{user}/following")
   end
 
   def self.find_organizations(user)
@@ -50,7 +47,7 @@ class GithubService
   end
 
   def find_organizations
-    get_url("/users/#{user}/orgs?client_id=#{ENV["GITHUB_CLIENT_ID"]}&client_secret=#{ENV["GITHUB_CLIENT_SECRET"]}")
+    get_url("/users/#{user}/orgs")
   end
 
   def self.find_repos(user)
@@ -58,7 +55,7 @@ class GithubService
   end
 
   def find_repos
-    get_url("/users/#{user}/repos?client_id=#{ENV["GITHUB_CLIENT_ID"]}&client_secret=#{ENV["GITHUB_CLIENT_SECRET"]}")
+    get_url("/users/#{user}/repos")
   end
 
   def self.find_events(user)
@@ -66,14 +63,8 @@ class GithubService
   end
 
   def find_events
-    get_events_url("/search/commits?q=author%3Akatiekeel&sort=author-date&per_page=800&client_id=#{ENV["GITHUB_CLIENT_ID"]}&client_secret=#{ENV["GITHUB_CLIENT_SECRET"]}")
-  end
-
-  def get_events_url(url)
-    response = @events_conn.get(url)
-    results = JSON.parse(response.body, symbolize_names: true)
-    new_results = []
-    results[:items].each {|commit| new_results << {:date => commit[:commit][:author][:date].to_date, :repo => commit[:repository][:name], :message => commit[:commit][:message]}}
+    results = get_url("/search/commits?q=author%3Akatiekeel&sort=author-date&per_page=800")
+    new_results = results[:items].map {|commit| {:date => commit[:commit][:author][:date].to_date, :repo => commit[:repository][:name], :message => commit[:commit][:message]}}
     new_results
   end
 
@@ -82,13 +73,11 @@ class GithubService
   end
 
   def find_following_events
-    results = get_url("/users/#{user}/following?client_id=#{ENV["GITHUB_CLIENT_ID"]}&client_secret=#{ENV["GITHUB_CLIENT_SECRET"]}")
-    urls = []
-    results.each {|result| urls << result[:events_url].split('{')[0]}
-    new_results = []
-    urls.each {|url| new_results << get_url(url + "?client_id=#{ENV["GITHUB_CLIENT_ID"]}&client_secret=#{ENV["GITHUB_CLIENT_SECRET"]}")}
-    new_results
+    results = get_url("/users/#{user}/following")
+    urls = results.map {|result| result[:events_url].split('{')[0]}
+    new_results = urls.map {|url| get_url(url)}
     newer_results = []
+    # I know nested each loops are bad, but I couldn't think of another way to extract the data I wanted
     new_results.each do |result|
       result.each do |new_result|
         newer_results << {:type => new_result[:type], :user => new_result[:actor][:login], :repo => new_result[:repo][:name].split('/')[-1], :date => new_result[:created_at].to_date}
